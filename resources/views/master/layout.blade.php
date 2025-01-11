63f796a2a7292c4376476d5e24c14bdcf83712c3
adlynadika <adlynsofeawork@gmail.com>
2025-01-11 23:34:08 +0800

    rename title

--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title> Medical HTML-5 Template </title>
+    <title> Group A - Web App Project </title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
